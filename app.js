6d67ecf9677f7687b00350ff08014e1886b17054
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-01-08 15:33:10 +1300

    Added nib library to extend Stylus (includes CSS3 mixin, etc)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var application_root = __dirname,
   express = require("express"),
   stylus = require("stylus"),
+  nib = require("nib"),
   path = require("path"),
   mongoose = require('mongoose');
 
@@ -14,12 +15,22 @@ var Item = mongoose.model('Item', new mongoose.Schema({
   order: Number
 }));
 
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib());
+}
+
 app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'All work and no play makes Jack a dull boy' }));
-  app.use(stylus.middleware({ src: __dirname + '/public' }));
+  app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+  }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
   app.set('views', __dirname + '/views');
@@ -30,7 +41,7 @@ app.configure(function(){
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {title: "MongoDB Backed TODO App"});
+  res.render('index', {title: "Sampling made simple ~ Sampleton"});
 });
 
 app.get('/api/items', function(req, res){
