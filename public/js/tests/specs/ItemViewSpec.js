6e5f4b68f7aacbaeea051358920ff4275545be85
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-01-17 14:57:31 +1300

    Added several behavior tests

--- public/js/tests/specs/ItemViewSpec.js
+++ public/js/tests/specs/ItemViewSpec.js
@@ -1,65 +1,135 @@
 define(["/js/views/item.view.js", "/js/models/item.model.js"], function(ItemView, Item) {
 
-  describe("Tests for ItemView", function() {
+  describe('Sampling item', function() {
     beforeEach(function() {
-        $('body').append('<ul id="item-list"></ul>');
-        this.itemView = new ItemView({model: new Item()});
+      $('body').append('<ul id="item-list"></ul>');
+      this.itemView = new ItemView({model: new Item()});
+
     });
 
     afterEach(function() {
-        this.itemView.remove();
-        $('#item-list').remove();
+      this.itemView.remove();
+      $('#item-list').remove();
     });
 
     //|-------|
     //| TESTS |
     //|-------|
 
-    it('Should be tied to a DOM element when created, based off the property provided.', function() {
-        //what html element tag name represents this view?
-        expect(this.itemView.el.tagName.toLowerCase()).toBe('li');
-    });
+    describe('Can use a MODEL', function() {
 
-    it('Is backed by a model instance, which provides the data.', function() {
-
+      it('should have a model', function() {
         expect(this.itemView.model).toBeDefined();
-        //| > Check that default variables are set
+      });
+      it('should have an empty title', function() {
         expect(this.itemView.model.get('title')).toMatch('');
+      });
+      it('should have a counter set to 0', function() {
         expect(this.itemView.model.get('counter')).toBe(0);
-        expect(this.itemView.model.get('order')).toBe(0);
+      });
     });
 
-    it('Can render, after which the DOM representation of the view will be visible.', function() {
-      this.itemView.render();
-      $("#item-list").append(this.itemView.el);
+    describe('Can RENDER', function(){
+      it('should be tied to an DOM element', function() {
+        expect(this.itemView.el.tagName.toLowerCase()).toBe('li');
+      });
 
-      expect($('#item-list').find('li').length).toBe(1);
+      it('should be in display mode', function() {
+        $('#item-list').append(this.itemView.render().el);
+        expect(this.itemView.el).not.toHaveClass('editing');
+      });
+
+      it('should render its title', function () {
+        var title = 'title';
+        $('#item-list').append(this.itemView.render().el);
+
+        this.itemView.model.set({title: title});
+        expect($(this.itemView.el).find('.item-title')).toHaveText(title);
+      });
+
+      it('should render its counter', function () {
+        var counter = 24;
+        $('#item-list').append(this.itemView.render().el);
+
+        this.itemView.model.set({counter: counter});
+        expect($(this.itemView.el).find('.item-counter')).toHaveText(counter);
+      });
+
     });
 
-    it('Can use an events hash to wire up view methods to DOM elements.', function() {
-        var viewElt;
+    describe('Can use EVENTS', function(){
 
-        runs(function() {
-            $('#item-list').append(this.itemView.render().el);
+      beforeEach(function () {
+        $('#item-list').append(this.itemView.render().el);
+        this.itemView.model.url = 'stub';
+      });
+
+      it('should increment counter WHEN clicking on the item', function(){
+        var item = $(this.itemView.el).find('.item');
+
+        spyOnEvent(item, 'click');
+        item.click();
+        expect('click').toHaveBeenTriggeredOn(item);
+        expect(this.itemView.model.get('counter')).toBe(1);
+      });
+
+      describe('Can use EVENTS on its TITLE', function(){
+
+        var title;
+        beforeEach(function () {
+          title = $(this.itemView.el).find('.item-title');
         });
 
-        runs(function() {
-
-          var counter = this.itemView.model.get('counter');
-
-          //| > We need to trick the .sync function
-          this.itemView.model.url = 'api/items';
-
-          $(this.itemView.el).find('.item').click();
-
-          expect(this.itemView.model.get('counter')).toBe(counter + 1);
+        it('should go in editing mode WHEN doubleclicking on the title', function(){
+          spyOnEvent(title, 'dblclick');
+          title.dblclick();
+          expect($(this.itemView.el)).toHaveClass('editing');
+          expect($(this.itemView.el).find('.item-input')).toBeVisible();
         });
-    });
 
+        it('should not increment WHEN clicking on title', function () {
+          title.click();
+          expect(this.itemView.model.get('counter')).toBe(0);
+        });
+
+      });
+
+      describe('Can use EVENTS on its INPUT', function(){
+
+        var input;
+        beforeEach(function () {
+          input = $(this.itemView.el).find('.item-input');
+        });
+
+        it('should save the input WHEN pressing enter', function(){
+          var newValue = 'my new value',
+              keypress = $.Event('keypress');
+
+          keypress.which = 13;
+
+          $(this.itemView.el).addClass('editing');
+
+          input.val(newValue);
+          input.trigger(keypress);
+
+          expect($(this.itemView.el)).not.toHaveClass('editing');
+          expect(this.itemView.model.get('title')).toBe(newValue);
+        });
+
+        it('should save the input WHEN losing focus', function(){
+          var newValue = 'my new value';
+
+          $(this.itemView.el).addClass('editing');
+
+          input.val(newValue);
+          input.trigger('blur');
+
+          expect($(this.itemView.el)).not.toHaveClass('editing');
+          expect(this.itemView.model.get('title')).toBe(newValue);
+        });
+      });
+
+    });
   });
 
-  
-	return {
-		name: "itemviewspec"
-	}
 });
