bdc8c70fa0ced3d16fcdabe3d0995ffa52f5f407
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-01-19 09:14:21 +1300

    Modified to inject dependencies in itemListView

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ require.config({
   paths: {
     jquery: 'libs/vendor/jquery/jquery-min',
     underscore: 'libs/vendor/underscore/underscore-min',
-    backbone: 'libs/vendor/backbone/backbone-optamd3-min',
+    backbone: 'libs/vendor/backbone/backbone',
     glasses: 'libs/glasses/glasses',
     text: 'libs/vendor/require/text',
     order: "libs/vendor/require/order"
@@ -10,6 +10,15 @@ require.config({
 
 });
 
-require(['views/itemList.view'], function(itemListView){
-  var itemList = new itemListView;
+require([
+  'views/itemList.view',
+  'views/item.view',
+  'collections/item.col'
+], function(itemListView, itemView, itemCollection){
+
+  var itemList = new itemListView({
+    rowView: itemView,
+    collection: new itemCollection()
+  });
+
 });
