b3a1f1d56cbdc4b47752310f3856a60301a2a99e
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-01-24 10:44:33 +1300

    Fixed keymap: typo on backspace and added quote to delete as reserved word

--- public/js/libs/glasses/glasses.util.js
+++ public/js/libs/glasses/glasses.util.js
@@ -47,14 +47,14 @@ define(['underscore'], function(_){
     },
     keyMap: {
       enter: 13,
-      backsoace: 8,
+      backspace: 8,
       tab: 9,
       escape: 27,
       left: 37,
       up: 38,
       right: 39,
       down: 40,
-      delete: 46
+      'delete': 46
     }
   };
 
