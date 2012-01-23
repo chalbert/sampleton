3321d3be8c185506b2b60803768e591db0022c74
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-01-23 15:25:35 +1300

    Typing in the field now filters the item list Click on clear empty the input and reset the list filter

--- public/js/views/searchbox.view.js
+++ public/js/views/searchbox.view.js
@@ -7,12 +7,56 @@ define([
 
   return o_o.view.extend({
 
-     className:  "searchbox",
+    el:  ".searchbox",
 
-    initialize: function(listView){
-      
-    }
+    elements: {
+      'input': '.search-input',
+      'reset': '.search-reset'
+    },
 
+    requirements: ['listView'],
+
+    initialize: function(){
+      this._super('initialize');
+      var input = this.$el('input');
+    },
+
+//------------------------------------------------------------------------
+
+   //|--------|
+   //| EVENTS |
+   //|--------|
+
+    events: function() {
+      return this.mapEvents({
+        input: 'keyup change',
+        reset: 'click'
+      });
+    },
+
+    input_change: function(e) {
+      this.search();
+    },
+
+    input_keyup: function(e) {
+      this.search();
+    },
+
+    reset_click: function(e) {
+      this.$el('input').val('');
+      this.listView.resetFilter();
+    },
+
+//------------------------------------------------------------------------
+
+   //|---------|
+   //| ACTIONS |
+   //|---------|
+
+  search: function(){
+    var search = this.$el('input').val();
+    this.listView.filterByTitle(search);
+  }
 
   });
 
