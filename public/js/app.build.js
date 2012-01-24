4362aa5eba3dadbda2e232e6092ec0605091869c
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-01-25 09:45:24 +1300

    First commit: profile for app building

---
+++ public/js/app.build.js
@@ -0,0 +1,18 @@
+({
+  appDir: "../",
+  baseUrl: "js",
+  dir: "../../public-build",
+  modules: [
+    {
+      name: "app"
+    }
+  ],
+  paths: {
+    jquery: 'libs/vendor/jquery/jquery-min',
+    underscore: 'libs/vendor/underscore/underscore-min',
+    backbone: 'libs/vendor/backbone/backbone-optamd3-min',
+    glasses: 'libs/glasses/glasses',
+    text: 'libs/vendor/require/text',
+    order: "libs/vendor/require/order"
+  }
+})
