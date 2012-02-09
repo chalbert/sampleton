96f87ab593df8eeb2719b5622ab6c44f0a926f06
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-02-10 11:56:51 +1300

    Added router to manage app states

---
+++ public/js/routers/app.router.js
@@ -0,0 +1,38 @@
+define(['backbone','underscore', 'mediator'], function(Backbone, _, mediator){
+  return Backbone.Router.extend({
+    routes: {
+      "": "editing",
+      "recording": "recording",
+      "records/:item": "records"
+    },
+
+    initialize: function(){
+
+      _.forEach(this.routes, function(name, route) {
+        mediator.subscribe(name + ':go', function(){
+          var matches = route.match(/:\w+/g),
+              args = Array.prototype.slice.call(arguments);
+          _.forEach(matches, function(match){
+            route = route.replace(match, args.shift());
+          });
+          this.navigate(route, {trigger: true});
+        }, this);
+      }, this);
+
+    },
+
+    recording: function(){
+      mediator.publish('editing:stop');
+    },
+
+    editing: function(){
+      mediator.publish('editing:start');
+      mediator.publish('records:close');
+    },
+
+    records: function(itemId){
+      mediator.publish('records:load', itemId);
+      mediator.publish('records:open', itemId);
+    }
+  });
+});
