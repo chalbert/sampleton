b89a7947a3b3db01bf5170a639ad7baf4994553d
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-02-10 11:58:07 +1300

    Added model to lib

--- public/js/libs/glasses/glasses.js
+++ public/js/libs/glasses/glasses.js
@@ -1,15 +1,17 @@
 //|   +---------+     +---------+
-//|   |         |     |         |
+//|   |       ` |     |       ` |
 //|   |         |-----|         |
 //|   |         |     |         |
 //|   +---------+     +---------+
 
 define([
   'libs/glasses/glasses.util',
-  'libs/glasses/glasses.view'
-  ], function(util, view){
+  'libs/glasses/glasses.view',
+  'libs/glasses/glasses.model'
+  ], function(util, view, model){
   return {
     util: util,
-    view: view
+    view: view,
+    model: model
   }
 });
