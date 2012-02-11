366df6934edc48ba2f38255888d320d70b926370
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-02-12 11:59:10 +1300

    Added trash control

--- public/js/views/controls.view.js
+++ public/js/views/controls.view.js
@@ -3,7 +3,8 @@ define([
   'underscore',
   'backbone',
   'glasses',
-  'mediator'
+  'mediator',
+  'jqueryui/droppable'
 ], function ($, _, Backbone, o_o, mediator) {
 
   return o_o.view.extend({
@@ -12,11 +13,23 @@ define([
 
     elements: {
       'start': '.control-start',
-      'pause': '.control-pause'
+      'pause': '.control-pause',
+      'trash': '.control-trash'
     },
 
     initialize: function(){
       this._super('initialize');
+
+      this.$trash.droppable({
+        tolerance: 'pointer',
+        hoverClass: 'active'
+      });
+
+      // Don't bubble, so need to attach directly on element
+      this.$trash
+          .bind('drop', $.proxy(this.trash_drop))
+          .bind('dropover', $.proxy(this.trash_dropover))
+          .bind('dropout', $.proxy(this.trash_dropout));
 
       mediator.subscribe('recording:start', this.pushStart, this);
       mediator.subscribe('recording:stop', this.pushPause, this);
@@ -33,6 +46,21 @@ define([
         start: 'click',
         pause: 'click'
       });
+    },
+
+    trash_dropover: function(e, ui) {
+//      this.$trash.addClass('active');
+      console.log('dropover');
+    },
+
+    trash_dropout: function(e, ui) {
+      console.log('dropout');
+//      this.$trash.removeClass('active');
+    },
+
+    trash_drop: function(e, ui){
+      mediator.publish('items:stopSorting');
+      mediator.publish('items:deleteItem', ui);
     },
 
     start_click: function(e) {
