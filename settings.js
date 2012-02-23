3044a93355c54ee5e6e3895a055c87e4d7296817
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-02-23 13:26:43 +1300

    Moved settings here from index.js

---
+++ settings.js
@@ -0,0 +1,38 @@
+//|----------|
+//| SETTINGS |
+//|----------|
+
+var MongoStore = require('connect-mongo');
+
+var settings = {}
+settings.db = {
+  db: 'sampleton',
+  host: 'localhost'
+};
+
+settings.session = {
+  secret: 'All work and no play makes Jack a dull boy',
+  maxAge : new Date(Date.now() + 3600000),
+  store: new MongoStore(settings.db)
+};
+
+var stylus = require("stylus"),
+    nib = require("nib");
+
+settings.stylus = {
+  src:__dirname + '/public',
+  compile: function (str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .set('compress', true)
+        .use(nib());
+  }
+};
+
+settings.paths = {
+  static: __dirname + '/public',
+  views: __dirname + '/templates'
+}
+
+
+module.exports = settings;
