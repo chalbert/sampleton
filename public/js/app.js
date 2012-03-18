75b5f65e6325e3ca2d19b5406f0971ace9c74f35
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-03-18 17:33:59 +1300

    Added commons scripts to app to be loaded once only

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,20 @@ require.config({
 
 require([
   'sampleton/app/app.view',
-  'sampleton/app/app.router'
+  'sampleton/app/app.router',
+  //COMMONS
+  'text', 'date',
+  'src/mixins/views/searchable.mixin',
+  'src/mixins/views/list.mixin',
+  'src/mixins/collections/orderable.mixin',
+  'src/ui/new.view',
+  'js/libs/vendor/jqueryui/core.js',
+  'js/libs/vendor/jqueryui/widget.js',
+  'js/libs/vendor/jqueryui/mouse.js',
+  'js/libs/vendor/jqueryui/draggable.js',
+  'js/libs/vendor/jqueryui/droppable.js',
+  'js/libs/vendor/jqueryui/sortable.js',
+  'touchpunch'
 ], function(appView, appRouter){
 
   var app = new appView(),
