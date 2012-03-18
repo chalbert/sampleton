99a452e669faebb578c78a4d0a44908ec72f0f27
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-03-19 09:43:17 +1300

    Fixed trailing comma

--- modules/models/user.M.js
+++ modules/models/user.M.js
@@ -5,7 +5,7 @@ var User = mongoose.model('User', new mongoose.Schema({
   role: {type: Number, 'default': 1 },
   email: String,
   name: String,
-  password: String,
+  password: String
 }));
 
 module.exports = User;
