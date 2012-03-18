085dfd41f3f3ddf82efd71d1bc6ed6ccbb5ba688
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-03-18 17:33:36 +1300

    Added optmization for each page

--- public/js/app.build.js
+++ public/js/app.build.js
@@ -1,3 +1,5 @@
+// node r.js -o public/js/app.build.js
+
 ({
   appDir: "../",
   baseUrl: "js",
@@ -5,13 +7,39 @@
   modules: [
     {
       name: "app"
+    },
+    {
+      name: 'sampleton/projects/projects.view',
+      exclude: ["app"]
+    },
+    {
+      name: 'sampleton/project/project.view',
+      exclude: ["app"]
+    },
+    {
+      name: 'sampleton/records/records.view',
+      exclude: ["app"]
+    },
+    {
+      name: 'sampleton/templates/templates.view',
+      exclude: ["app"]
+    },
+    {
+      name: 'sampleton/template/template.view',
+      exclude: ["app"]
     }
   ],
   paths: {
-    jquery: 'libs/vendor/jquery/jquery-min',
-    underscore: 'libs/vendor/underscore/underscore-min',
-    backbone: 'libs/vendor/backbone/backbone-optamd3-min',
+    jquery: 'libs/vendor/jquery/jquery',
+    jqueryui: 'libs/vendor/jqueryui',
+    touchpunch: 'libs/vendor/jqueryui/jquery.ui.touch-punch.min',
+    cookie: 'libs/vendor/jquery/jquery.cookie',
+    underscore: 'libs/vendor/underscore/underscore-extended',
+    backbone: 'libs/vendor/backbone/backbone-extended',
     text: 'libs/vendor/require/text',
-    order: "libs/vendor/require/order"
+    order: "libs/vendor/require/order",
+    date: "libs/vendor/date/date",
+    modelBinding: "libs/vendor/backbone/backbone.modelbinding",
+    handlebars: "empty:"
   }
 })
