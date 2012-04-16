ff3759d8a54c278542f386eea7bff3b6f4e274ef
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-04-16 14:59:40 +1200

    Improved pre-loading of common dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,21 @@
 require.config({
   paths: {
     jquery: 'libs/vendor/jquery/jquery',
-      clickout: 'src/plugins/jquery/jquery-clickout/jquery.clickout',
+    clickout: 'src/plugins/jquery/jquery-clickout/jquery.clickout',
     jqueryui: 'libs/vendor/jqueryui',
     touchpunch: 'libs/vendor/jqueryui/jquery.ui.touch-punch.min',
     cookie: 'libs/vendor/jquery/jquery.cookie',
-    underscore: 'libs/vendor/underscore/underscore-extended',
-      'underscore-keys': 'src/plugins/underscore/underscore-keys/underscore-keys',
-    backbone: 'libs/vendor/backbone/backbone-extended',
-      'backbone_super': 'libs/vendor/backbone/backbone_super',
-      'backbone-mixins': 'src/plugins/backbone/backbone-mixins/backbone-mixins',
-      'backbone-elements': 'src/plugins/backbone/backbone-elements/backbone-elements',
-      'backbone-multiviews': 'src/plugins/backbone/backbone-multiviews/backbone-multiviews',
-      'backbone-mediator': 'src/plugins/backbone/backbone-mediator/backbone-mediator',
-      'backbone-shortcuts': 'src/plugins/backbone/backbone-shortcuts/backbone-shortcuts',
+    underscore: 'libs/vendor/underscore/underscore',
+    'underscore-keys': 'src/plugins/underscore/underscore-keys/underscore-keys',
+    backbone: 'libs/vendor/backbone/backbone',
+    'backbone_super': 'libs/vendor/backbone/backbone_super',
+    'backbone-mixins': 'src/plugins/backbone/backbone-mixins/backbone-mixins',
+    'backbone-elements': 'src/plugins/backbone/backbone-elements/backbone-elements',
+    'backbone-keyevents': 'src/plugins/backbone/backbone-keyevents/backbone-keyevents',
+    'backbone-multiviews': 'src/plugins/backbone/backbone-multiviews/backbone-multiviews',
+    'backbone-multiviews-bindings': 'src/plugins/backbone/backbone-multiviews-bindings',
+    'backbone-mediator': 'src/plugins/backbone/backbone-mediator/backbone-mediator',
+    'backbone-shortcuts': 'src/plugins/backbone/backbone-shortcuts/backbone-shortcuts',
     text: 'libs/vendor/require/text',
     order: "libs/vendor/require/order",
     date: "libs/vendor/date/date",
@@ -23,16 +25,34 @@ require.config({
 
 });
 
+// PLUGINS
 require([
-  'preload',
-  'sampleton/app/app.view',
-  'sampleton/app/app.router'
-], function($, appView, appRouter){
+  // BACKBONE
+  'backbone_super',
+  'backbone-mixins',
+  'backbone-mediator',
+  'src/plugins/backbone/backbone-baseModel',
+  'src/plugins/backbone/backbone-baseCollection',
+  'backbone-elements',
+  'backbone-keyevents',
+  'backbone-multiviews',
+  'backbone-multiviews-bindings',
+  'backbone-shortcuts',
+  // UNDERSCORE
+  'src/plugins/underscore/underscore-extension',
+  'underscore-keys'
+], function(){
 
-  var app = new appView(),
-      router = new appRouter();
-  app.open();
+  require([
+    'preload',
+    'sampleton/app/app.view',
+    'sampleton/app/app.router'
+  ], function($, appView, appRouter){
 
+    var app = new appView(),
+        router = new appRouter();
+    app.open();
 
+  });
 
 });
