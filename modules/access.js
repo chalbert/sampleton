e5e712544006aa74a0d5f008fd4c638cfdc2278a
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-06-23 10:31:42 +1200

    Fixed bug on the option checking

--- modules/access.js
+++ modules/access.js
@@ -8,7 +8,7 @@ access.isRestrictedTo = function(role, options) {
 
   return function(req, res, next) {
 
-    if (req.session.user && req.session.user.authenticated) {
+    if (req.session && req.session.user && req.session.user.authenticated) {
 
       if (req.session.user.role >= roles[role]) {
         next();
