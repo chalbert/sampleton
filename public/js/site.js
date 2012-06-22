f9ed7bd3dbdd70e25bce1768d70ad73efc72a9a6
Nicolas Gilbert <nic.g.gilbert@gmail.com>
2012-06-23 10:53:21 +1200

    Added effects & anim on logon screen

--- public/js/site.js
+++ public/js/site.js
@@ -8,7 +8,8 @@ require.config({
     '2D': 'libs/vendor/intersection/2D',
     intersection: 'libs/vendor/intersection/Intersection',
     jqueryHooks: 'src/plugins/jquery/jquery-hooks/jquery-hooks',
-    jqueryHooksTransform: 'libs/vendor/jquery/jquery-hook-transform'
+    jqueryHooksTransform: 'libs/vendor/jquery/jquery-hook-transform',
+    debug: 'src/debug'
   }
 });
 
@@ -19,7 +20,11 @@ require([
 ], function($){
   'use strict';
 
+  window.Text
+
   $(function(){
+
+
     var elements = {
           info: '.info',
           login: '#login',
@@ -31,7 +36,8 @@ require([
         $content = $(elements.content),
         $details = $(elements.details),
         action = window.Touch ? 'touchstart' : 'click',
-        center, setCenter;
+        center, setCenter, setRotation, previousCoor,
+        threshold, max;
 
     $login.on(action, elements.info, function(e){
       e.preventDefault();
@@ -50,27 +56,79 @@ require([
 
     if (document.location.hash === '#info') $info.click();
 
-    setCenter = function() {
-      center = {
-        x: $(window).width() / 2,
-        y: $(window).height() / 2
-      };
-    }.call(this);
-
-    $(window).resize(function(){
-      setCenter();
-    });
-
-    $('body').bind('mousemove', function(e){
-      if ($login.hasClass('flip')) return;
-      var rotateY = ((e.pageX - center.x) / center.x) * 10,
-          rotateX = ((e.pageY - center.y) / center.y) * -10;
+    setRotation = function(x, y){
+      if ($login.hasClass('flip')) {
+        y = y -180;
+        x *= -1;
+      }
 
       $login.css({
-        transform: 'rotateY(' + rotateY + 'deg) rotateX(' + rotateX + 'deg)'
+        transform: 'rotateY(' + y + 'deg) rotateX(' + x + 'deg)'
+      });
+    };
+
+    // The perspective effect is applied only for mouse devices
+    if (!window.Touch) {
+      setCenter = function() {
+        center = {
+          x: $(window).width() / 2,
+          y: $(window).height() / 2
+        };
+      }.call(this);
+
+      $(window).resize(function(){
+        setCenter();
       });
 
-    });
+      $('body').bind('mousemove', function(e){
+        if ($login.hasClass('flip')) return;
+        var rotateY = ((e.pageX - center.x) / center.x) * 10,
+            rotateX = ((e.pageY - center.y) / center.y) * -10;
+
+        setRotation(rotateX, rotateY);
+
+      });
+    }
+
+    if(window.DeviceMotionEvent) {
+      previousCoor = {x: 0, y: 0, z: 0};
+
+     max = 15;
+     threshold = 0.2;
+
+     $(window).on('devicemotion', function(e) {
+       var coor, acc, rotateY, rotateX;
+
+       coor = e.originalEvent.accelerationIncludingGravity;
+
+       if (Math.abs(coor.x - previousCoor.x) < threshold) coor.x = previousCoor.x;
+       if (Math.abs(coor.y - previousCoor.y) < threshold) coor.y = previousCoor.y;
+
+       previousCoor = coor;
+
+       if (window.orientation === 0) {
+         rotateX = coor.y * 5;
+         rotateY = coor.x * 5;
+       } else {
+         rotateX = coor.x * -5;
+         rotateY = coor.y * 5;
+       }
+
+       if(rotateX > max) {
+         rotateX = max;
+       } else if(rotateX < -max) {
+         rotateX = -max;
+       }
+       if(rotateY > max) {
+         rotateY = max;
+       } else if(rotateY < -max) {
+         rotateY = -max;
+       }
+
+       setRotation(rotateX, rotateY);
+
+     });
+    }
   });
 
 });
